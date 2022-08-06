03b3a3bff21d479b96aceb2471202467fab55fd9
manapixels <chrenelias@outlook.com>
2022-08-06 22:40:44 +0800

    Update landing page

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { AppProps } from 'next/app'
+import { extendTheme } from '@chakra-ui/react'
+import '../styles/globals.css'
+
+const colors = {
+   brand: {
+      900: '#1a365d',
+      800: '#153e75',
+      700: '#2a69ac',
+   },
+}
+
+const theme = extendTheme({ colors })
+
+function MyApp({ Component, pageProps }: AppProps) {
+   return (
+      <ChakraProvider theme={theme}>
+         <Component {...pageProps} />
+      </ChakraProvider>
+   )
+}
+
+export default MyApp
